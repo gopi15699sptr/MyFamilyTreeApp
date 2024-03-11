c038d062ea4aa388093603a829de48305b67da7e
Ullas-A-2000 <Ullas.A@eligarf.tech>
2024-03-11 22:46:50 +0530

    push spouse changes

--- App.jsx
+++ App.jsx
@@ -5,13 +5,13 @@ import {Path, G, Circle, Rect, Text as SvgText, Svg} from 'react-native-svg';
 
 const App = () => {
   const data = [
-    {name: 'Grandparent', parent: ''},
-    {name: 'Parent 1', parent: 'Grandparent', spouses: ['Wife 1']},
-    {name: 'Child 1', parent: 'Parent 1'},
-    {name: 'Child 2', parent: 'Parent 1'},
-    {name: 'Parent 2', parent: 'Grandparent', spouses: ['Wife 2']},
+    {name: 'Grandparent', parent: '', spouses: ['Wife 1'],children:2},
+    {name: 'Parent 1', parent: 'Grandparent', spouses: ['Wife 1'],children:null},
+    // {name: 'Child 1', parent: 'Parent 1'},
+    {name: '', parent: 'Parent 1'},
+    {name: 'Parent 2', parent: 'Grandparent', spouses: ['Wife 2'],children:2},
     {name: 'Child 3', parent: 'Parent 2'},
-    {name: 'Child 4', parent: 'Parent 2', spouses: ['Wife 3']},
+    {name: 'Child 4', parent: 'Parent 2', spouses: ['Wife 3'],children:2},
     {name: 'Child 5', parent: 'Child 4'},
     {name: 'Child 6', parent: 'Child 4'},
   ];
@@ -21,91 +21,96 @@ const App = () => {
     .id(d => d.name)
     .parentId(d => d.parent)(data);
 
-  const treeLayout = d3.tree().size([650, 200]);
+  const treeLayout = d3.tree().size([750, 200]);
   const nodes = treeLayout(treeData);
 
   return (
     <View style={{backgroundColor: '#4d4d4d', flex: 1}}>
       <View>
-        <Svg width={900} height={900} transform="translate(-100, 200)">
+        <Svg width={1200} height={900} transform="translate(-100, 200)">
           {nodes.links().map((link, index) => (
-            <Path
-              transform={{translateY: 5}}
-              key={index}
-              d={
-                'M' +
-                link.source.x +
-                ',' +
-                link.source.y +
-                'v 50 H' +
-                link.target.x +
-                'V' +
-                link.target.y
-              }
-              fill="none"
-              stroke="orange"
-            />
+            <>
+             {link.target.data.name && (
+                <Path
+                  transform={{translateY: 5}}
+                  key={index}
+                  d={
+                    'M' +
+                    (link.source.x - 20) +
+                    ',' +
+                  (link.source.y) +
+                    'h 60 v 50 H' +
+                    link.target.x +
+                    'V' +
+                    link.target.y
+                  }
+                  fill="none"
+                  stroke="orange"
+                />
+             )}
+            </>
           ))}
           {nodes.descendants().map((node, index) => (
             <G key={index} transform={{translateY: 20}}>
-              <Rect
-                x={node.x - 40}
-                y={node.y - 20}
-                fill="#4d4d4d"
-                width={80}
-                height={40}
-                strokeWidth={1}
-                stroke="silver"
-              />
+              {node.data.name && (
+                <Rect
+                  x={node.x - 60}
+                  y={node.y - 20}
+                  fill="#4d4d4d"
+                  width={80}
+                  height={40}
+                  strokeWidth={1}
+                  stroke="silver"
+                />
+              )}
 
+              {/* below is spouse code */}
               {Array.isArray(node.data.spouses) && (
                 <>
-                  {nodes.links().map((link, index) => (
-                    !link.source.data['spouses']  ? (
-                      <>
-                      <Path
-                        transform={{translateX: 40}}
-                        key={index}
-                        d={
-                          'M' +
-                          link.target.x +
-                          ',' +
-                          link.target.y + 'h 20'
-                        }
-                        fill="none"
-                        stroke="orange"
-                      />
-                      {/* <Path
-                        transform={{translateX: 50 ,translateY:36}}
-                        key={index+1}
-                        d={
-                          'M' +
-                          link.target.x +
-                          ',' +
-                          link.target.y + 'V 30'
-                        }
-                        fill="none"
-                        stroke="orange"
-                      /> */}
-                      </>
-                    ) : null
-                  ))}
-                  {node.data.spouses.map((spouse, spouseIndex) => (
-                    <G
-                      key={spouseIndex}
-                      transform={{translateY: 20, translateX: 20}}>
-                      <Rect
-                        x={node.x + 40}
-                        y={node.y - 40}
-                        fill="#4d4d4d"
-                        width={80}
-                        height={40}
-                        strokeWidth={1}
-                        stroke="silver"
-                      />
-                    </G>
-                  ))}
+              {/* spouse default connection when they have children */}
+              {node.data.children ? (
+                  <Path
+                    transform={{translateY: 0}}
+                    key={index}
+                    d={
+                      'M' +
+                      (node.x + 40) +
+                      ',' +
+                      (node.y - 15) +
+                      'h 20 '
+                    }
+                    fill="none"
+                    stroke="orange"
+                  />
+               ) : (
+                // spouse connection when they dont have children
+                    <Path
+                    transform={{translateY: 0}}
+                    key={index}
+                    d={
+                      'M' +
+                      (node.x + 20) +
+                      ',' +
+                      (node.y - 15) +
+                      'h 40 '
+                    }
+                    fill="none"
+                    stroke="orange"
+                  />
+               )}
+
+                  {/* below is spouse boxes */}
+                  <Rect
+                    x={node.x + 60}
+                    y={node.y - 20}
+                    fill="#4d4d4d"
+                    width={80}
+                    height={40}
+                    strokeWidth={1}
+                    stroke="silver"
+                />
                 </>
+                
               )}
             </G>
           ))}
